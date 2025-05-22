f2a26070d0a2fc138c01e5cdd85a7f37357ce6de
vlada890 <69687973+vlada890@users.noreply.github.com>
2025-05-22 22:53:31 +0300

    Update CourseListViewModel.cs

--- OnlineCourses.Model/ViewModels/CourseListViewModel.cs
+++ OnlineCourses.Model/ViewModels/CourseListViewModel.cs
@@ -10,6 +10,7 @@ namespace OnlineCourses.Model.ViewModels
     {
         public IEnumerable<CourseDto> Courses { get; set; }
         public bool IsAuthenticated { get; set; }
+        public string SearchTerm { get; set; }
     }
 
     public class CourseDto
