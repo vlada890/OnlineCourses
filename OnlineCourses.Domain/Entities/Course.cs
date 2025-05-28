9f2942333618fd220cd5faa72e5ee8a242c5c840
vlada890 <69687973+vlada890@users.noreply.github.com>
2025-05-28 15:26:21 +0300

    Update Course.cs

--- OnlineCourses.Domain/Entities/Course.cs
+++ OnlineCourses.Domain/Entities/Course.cs
@@ -5,7 +5,8 @@ namespace OnlineCourses.Domain.Entities
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string Instructor { get; set; }
+        public int InstructorId { get; set; }
+        public User Instructor { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
     }
 }
