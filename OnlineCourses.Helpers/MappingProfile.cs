45d91523ecc1ddcb3f5d68f22c23fc7a35015fad
vlada890 <69687973+vlada890@users.noreply.github.com>
2025-05-28 15:28:42 +0300

    Update MappingProfile.cs

--- OnlineCourses.Helpers/MappingProfile.cs
+++ OnlineCourses.Helpers/MappingProfile.cs
@@ -8,16 +8,38 @@ namespace OnlineCourses.Helpers
     {
         public MappingProfile()
         {
-            // Domain to DTO
+            // Domain to DTO mappings
             CreateMap<Course, CourseDto>();
             CreateMap<Course, CourseDetailsViewModel>();
+            CreateMap<Course, CourseManagementDto>()
+                .ForMember(dest => dest.EnrollmentCount, opt => opt.MapFrom(src => src.Enrollments.Count));
+        
+            CreateMap<User, UserDto>()
+                .ForMember(dest => dest.EnrollmentCount, opt => opt.MapFrom(src => src.Enrollments.Count));
+        
             CreateMap<Enrollment, EnrollmentDto>()
                 .ForMember(dest => dest.CourseTitle, opt => opt.MapFrom(src => src.Course.Title))
                 .ForMember(dest => dest.CourseInstructor, opt => opt.MapFrom(src => src.Course.Instructor));
-
-            // ViewModel to Domain
+        
+            // ViewModel to Domain mappings
             CreateMap<RegisterViewModel, User>()
-                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore());
+                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow)) ;
+        
+            CreateMap<CreateCourseViewModel, Course>();
+            CreateMap<EditCourseViewModel, Course>();
+        
+            // Domain to ViewModel mappings
+            CreateMap<User, EditUserViewModel>();
+            CreateMap<EditUserViewModel, User>()
+                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+                .ForMember(dest => dest.Enrollments, opt => opt.Ignore());
+        
+            CreateMap<Course, EditCourseViewModel>();
+            CreateMap<Course, CourseAdminViewModel>()
+            .ForMember(dest => dest.EnrollmentCount, opt => opt.MapFrom(src => src.Enrollments.Count))
+            .ForMember(dest => dest.Duration, opt => opt.Ignore())    // since Course doesn't have Duration
+            .ForMember(dest => dest.CreatedDate, opt => opt.Ignore());
         }
     }
 }
