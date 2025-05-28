0c7fdc89b7e362b3c7499f037ab072b6962034e8
vlada890 <69687973+vlada890@users.noreply.github.com>
2025-05-28 15:39:42 +0300

    Update SessionKeys.cs

--- OnlineCourses.Web/Constants/SessionKeys.cs
+++ OnlineCourses.Web/Constants/SessionKeys.cs
@@ -5,5 +5,6 @@ namespace OnlineCourses.Web.Constants
         public const string UserId = "UserId";
         public const string UserName = "UserName";
         public const string UserEmail = "UserEmail";
+        public const string UserRole = "UserRole";
     }
 }
