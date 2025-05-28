22a7057489d9e21daf915a25de49c75cbc9c4001
vlada890 <69687973+vlada890@users.noreply.github.com>
2025-05-28 15:17:19 +0300

    Update CourseRepository.cs

--- OnlineCourses.Data/Repositories/CourseRepository.cs
+++ OnlineCourses.Data/Repositories/CourseRepository.cs
@@ -24,6 +24,27 @@ namespace OnlineCourses.Data.Repositories
         {
             return await _ctx.SaveChangesAsync() > 0;
         }
-
+                public async Task AddAsync(Course course)
+        {
+            await _ctx.Courses.AddAsync(course);
+        }
+        public async Task DeleteAsync(int courseId)
+        {
+            var course = await GetByIdAsync(courseId);
+            if (course != null)
+            {
+                _ctx.Courses.Remove(course);
+            }
+        }
+        public async Task<IEnumerable<Course>> GetCoursesByInstructorAsync(int instructorId)
+        {
+            return await _ctx.Courses
+                .Where(c => c.InstructorId == instructorId)
+                .ToListAsync();
+        }
+        public async Task Update(Course course)
+        {
+            _ctx.Courses.Update(course);
+        }
     }
 }
