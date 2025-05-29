8f620137202fff840ab382d69b2dc6d2398f0c4a
vlada890 <69687973+vlada890@users.noreply.github.com>
2025-05-29 17:40:37 +0300

    Update RegisterViewModel.cs

--- OnlineCourses.Model/ViewModels/RegisterViewModel.cs
+++ OnlineCourses.Model/ViewModels/RegisterViewModel.cs
@@ -31,8 +31,6 @@ namespace OnlineCourses.Model.ViewModels
         [Display(Name = "Confirm Password")]
         public string ConfirmPassword { get; set; }
 
-        public string ConfirmPassword { get; set; }
-
         [Display(Name = "Role")]
         public UserRole Role { get; set; } = UserRole.Student;
     }
