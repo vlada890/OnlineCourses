760b34e5d8c19f18515e8c3f23438fc7f345ebfe
vlada890 <69687973+vlada890@users.noreply.github.com>
2025-05-29 16:31:50 +0300

    Update ICourseService.cs

--- OnlineCourses.BusinessLogic/Interfaces/ICourseService.cs
+++ OnlineCourses.BusinessLogic/Interfaces/ICourseService.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OnlineCourses.Domain.Entities;
+using OnlineCourses.Model.Common;
 using OnlineCourses.Model.ViewModels;
 
 namespace OnlineCourses.BusinessLogic.Interfaces
 {
     public interface ICourseService
     {
-        Task<IEnumerable<CourseDto>> GetAllCoursesAsync(int? userId = null, string searchTerm = null);
+        Task<IEnumerable<CourseDto>> GetAllCoursesAsync(int? userId = null);//, string searchTerm = null);
         Task<CourseDetailsViewModel> GetCourseByIdAsync(int id, int? userId = null);
         Task<bool> EnrollAsync(int userId, int courseId);
         Task<UserEnrollmentsViewModel> GetUserEnrollmentsAsync(int userId);
