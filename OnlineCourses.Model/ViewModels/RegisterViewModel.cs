c86681430f800aca9e5ee251d5867c9beb21e3bc
vlada890 <69687973+vlada890@users.noreply.github.com>
2025-05-29 17:15:29 +0300

    Update RegisterViewModel.cs

--- OnlineCourses.Model/ViewModels/RegisterViewModel.cs
+++ OnlineCourses.Model/ViewModels/RegisterViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OnlineCourses.Domain.Entities;
 using System.ComponentModel.DataAnnotations;
 
 namespace OnlineCourses.Model.ViewModels
