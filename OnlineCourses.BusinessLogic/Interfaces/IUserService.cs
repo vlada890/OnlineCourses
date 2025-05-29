92d071490725c4cbc4f0867c52e99b8964829399
vlada890 <69687973+vlada890@users.noreply.github.com>
2025-05-29 16:33:16 +0300

    Update IUserService.cs

--- OnlineCourses.BusinessLogic/Interfaces/IUserService.cs
+++ OnlineCourses.BusinessLogic/Interfaces/IUserService.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OnlineCourses.Model.Common;
 using OnlineCourses.Domain.Entities;
 using OnlineCourses.Model.ViewModels;
 
